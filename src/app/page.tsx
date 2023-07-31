cbd311df7b96b31ddf0f99995caea3e109bcb7a2
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2023-07-31 12:52:09 +0900

    fix something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ export default function Home({ searchParams: { page } }: { searchParams: { page:
   return (
     <main className='container max-w-screen-lg my-10'>
       <div className='flex flex-col gap-6 text-center justify-center'>
-        <AspectRatio ratio={15 / 4} className='bg-white bg-center bg-cover rounded-lg'/>
-        <h1 className='text-3xl font-black'>リーダーボード</h1>
+        <AspectRatio ratio={15 / 4} className='bg-[url(/banner.png)] bg-center bg-cover rounded-lg'/>
+        <h1 className='text-3xl font-black'>NONICK SERVER リーダーボード</h1>
       </div>
 
       <div className='py-12 mx-auto'>
         <Suspense fallback={(
           <div className='flex py-20 justify-center'>
-            <AiOutlineLoading3Quarters className='animate-spin' size={20}/>
+            <AiOutlineLoading3Quarters className='animate-spin' size={30}/>
           </div>
         )}>
-          <LeaderBoard skip={(Number(page) - 1) * 50}/>
+          <LeaderBoard skip={((Number(page) || 1) - 1) * 50}/>
         </Suspense>
       </div>
     </main>
