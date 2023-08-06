8f4c885e1f504f0e54f2a0c9ccaa604aa5ce6210
akki256 <aki256@outlook.com>
2023-08-06 22:30:05 +0900

    Remove unnecessary properties

--- src/components/levelCardWrapper.tsx
+++ src/components/levelCardWrapper.tsx
@@ -3,12 +3,11 @@
 import { FadeUpCard } from "./animation";
 import { ReactNode } from "react";
 
-export async function LevelCardWrapper({ children, index, isAdmin }: { index: number, isAdmin: boolean, children: ReactNode }) {
+export async function LevelCardWrapper({ children, isAdmin }: { isAdmin: boolean, children: ReactNode }) {
   return (
     <>
       <FadeUpCard
         className='px-6 py-4 flex gap-4 items-center'
-        key={index}
       >
         {children}
       </FadeUpCard>
