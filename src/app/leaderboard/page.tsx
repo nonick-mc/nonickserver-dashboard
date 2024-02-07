6555f8afce141b0774dce9412b43c23c637e8d22
akki256 <aki256@outlook.com>
2024-02-08 01:50:26 +0900

    use mongo data

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -14,91 +14,9 @@ export const metadata: Metadata = {
 export default async function Page({
   searchParams: { page },
 }: { searchParams: { page: string } }) {
-  // await dbConnect();
-  // const levels = await getLevelData();
-  //testData
-  const levels = [
-    {
-      id: '841942479446999081',
-      lv: 82,
-      xp: 19187,
-      boost: 1,
-      last: new Date(),
-      rank: 1,
-    },
-    {
-      id: '735110742222831657',
-      lv: 81,
-      xp: 15460,
-      boost: 1,
-      last: new Date(),
-      rank: 2,
-    },
-    {
-      id: '1021694511644155965',
-      lv: 77,
-      xp: 22469,
-      boost: 1,
-      last: new Date(),
-      rank: 3,
-    },
-    {
-      id: '695420898961784913',
-      lv: 71,
-      xp: 24956,
-      boost: 1,
-      last: new Date(),
-      rank: 4,
-    },
-    {
-      id: '973951195183972402',
-      lv: 67,
-      xp: 25366,
-      boost: 1,
-      last: new Date(),
-      rank: 5,
-    },
-    {
-      id: '517268384619036697',
-      lv: 67,
-      xp: 10957,
-      boost: 1,
-      last: new Date(),
-      rank: 6,
-    },
-    {
-      id: '831859839911067730',
-      lv: 66,
-      xp: 14996,
-      boost: 1,
-      last: new Date(),
-      rank: 7,
-    },
-    {
-      id: '947315844235554846',
-      lv: 65,
-      xp: 2906,
-      boost: 1,
-      last: new Date(),
-      rank: 8,
-    },
-    {
-      id: '928273042671558656',
-      lv: 62,
-      xp: 7845,
-      boost: 1,
-      last: new Date(),
-      rank: 9,
-    },
-    {
-      id: '845214644658110484',
-      lv: 61,
-      xp: 6949,
-      boost: 1,
-      last: new Date(),
-      rank: 10,
-    },
-  ];
+  await dbConnect();
+  const levels = await getLevelData();
+  const skip = ((Number(page) || 1) - 1) * 50;
   return (
     <>
       <div className='flex flex-col gap-6 text-center justify-center'>
@@ -123,9 +41,7 @@ export default async function Page({
           <h3 className='font-black text-2xl'>みんなの順位</h3>
           <PageSelector size={levels.length} />
         </div>
-        <Leaderboard
-          levels={levels.slice(((Number(page) || 1) - 1) * 50, 50)}
-        />
+        <Leaderboard levels={levels.slice(skip, skip + 50)} />
       </div>
     </>
   );
